44b6aeb03c4733bd6e63cc741558ee002bc452d8
DrinkingWater64 <aamuti64@gmail.com>
2022-01-29 23:23:53 +0600

    fixed few hard coded property

--- public/js/index.js
+++ public/js/index.js
@@ -45513,17 +45513,40 @@ class Slider {
 
 	handle;
 
+	radius = 8;
+
 	app;
+
+	constructor(app) {
+		this.app = app;
+
+		if (!('events' in app.renderer)) {
+			app.renderer.addSystem(_pixi_events__WEBPACK_IMPORTED_MODULE_1__.EventSystem, 'events');
+		}
+
+		// Make the slider
+		this.slider = app.stage.addChild(new pixi_js__WEBPACK_IMPORTED_MODULE_0__.Graphics().beginFill(0x38404e, 0.87).drawRect(this.xpos, this.ypos, this.width, this.height).endFill());
+		// Add invisible scrolling area that's wider than visible slider.
+		this.slider.beginFill(0xffffff, 0.001).drawRect(this.xpos, this.ypos, this.width, this.height).endFill();
+		this.slider.interactive = true;
+		this.slider.addEventListener('wheel', this.onWheel);
+		// Draw the handle
+		this.handle = this.slider.addChild(new pixi_js__WEBPACK_IMPORTED_MODULE_0__.Graphics().beginFill(0x650a5a).drawCircle(this.xpos, this.ypos*2, this.radius).endFill());
+		this.handle.interactive = true;
+
+		this.handle.addEventListener('pointerdown', this.onDragStart);
+		this.handle.addEventListener('pointerup', this.onDragEnd);
+		this.handle.addEventListener('pointerupoutside', this.onDragEnd);
+	}
 
 	onHandleMoved = () => {
 		// Normalize handle position between -1 and 1.
 		const t = 2 * (this.handle.position.y / this.app.renderer.screen.height - 0.5);
-		// bunny.scale.set(3 * (1.1 + t));
 	};
 
 	onWheel = (e) => {
 		const deltaY = e.deltaY;
-		this.handle.position.x = Math.max(4, Math.min(this.handle.position.x + deltaY, 200 - 4));
+		this.handle.position.x = Math.max(this.radius/2, Math.min(this.handle.position.x + deltaY, 200 - this.radius/2));
 		this.onHandleMoved();
 
 		e.preventDefault();
@@ -45531,7 +45554,7 @@ class Slider {
 
 	onDrag = (e) => {
 		// Set handle y-position to match pointer, clamped to (4, screen.height - 4).
-		this.handle.position.x = Math.max(4, Math.min(this.slider.toLocal(e.global).x, 200 - 4));
+		this.handle.position.x = Math.max(this.radius/2, Math.min(this.slider.toLocal(e.global).x, 200 - this.radius/2));
 		this.onHandleMoved();
 	};
 
@@ -45544,28 +45567,6 @@ class Slider {
 		this.app.stage.interactive = false;
 		this.app.stage.removeEventListener('pointermove', this.onDrag);
 	};
-
-	constructor(app) {
-		this.app = app;
-
-		if (!('events' in app.renderer)) {
-			app.renderer.addSystem(_pixi_events__WEBPACK_IMPORTED_MODULE_1__.EventSystem, 'events');
-		}
-
-		// Make the slider
-		this.slider = app.stage.addChild(new pixi_js__WEBPACK_IMPORTED_MODULE_0__.Graphics().beginFill(0x38404e, 0.87).drawRect(10, 10, 200, 20).endFill());
-		// Add invisible scrolling area that's wider than visible slider.
-		this.slider.beginFill(0xffffff, 0.001).drawRect(10, 10, 200, 20).endFill();
-		this.slider.interactive = true;
-		this.slider.addEventListener('wheel', this.onWheel);
-		// Draw the handle
-		this.handle = this.slider.addChild(new pixi_js__WEBPACK_IMPORTED_MODULE_0__.Graphics().beginFill(0x650a5a).drawCircle(10, 10, 8).endFill());
-		this.handle.interactive = true;
-		this.handle.position.set(10, 10);
-		this.handle.addEventListener('pointerdown', this.onDragStart);
-		this.handle.addEventListener('pointerup', this.onDragEnd);
-		this.handle.addEventListener('pointerupoutside', this.onDragEnd);
-	}
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Slider);
